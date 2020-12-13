34eb0eab4e5aa3f41135d7fac20623d46a6261a7
Tanvir-Aunjum-Sunny <sk375478771@gmail.com>
2020-12-14 03:42:45 +0600

    changed again

--- src/settings.211c2.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/_scene/main.fire",orientation:"landscape",jsList:["assets/_Script/fbinstant.6.3.364e4.js","assets/_Script/sdk.59a83.js"],bundleVers:{internal:"279e0",main:"d43fe"}};
